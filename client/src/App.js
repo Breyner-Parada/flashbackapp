a7a226d965275fa9999ac884f05fb365450025b5
Breyner Parada <paradabre@gmail.com>
2024-03-19 14:03:42 -0500

    url updated

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,11 @@ function App() {
       <Container maxWidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/flashback" element={<Navigate to='/flashback/posts' />} />
-          <Route path="/flashback/posts" element={<Home />} />
-          <Route path="/flashback/posts/search" element={<Home />} />
-          <Route path="/flashback/posts/:id" element={<PostDetails />} />
-          <Route path="/auth" element={!user ? <Auth />  : <Navigate to='/flashback/posts' />} />
+          <Route path="/flashbackapp" element={<Navigate to='/flashbackapp/posts' />} />
+          <Route path="/flashbackapp/posts" element={<Home />} />
+          <Route path="/flashbackapp/posts/search" element={<Home />} />
+          <Route path="/flashbackapp/posts/:id" element={<PostDetails />} />
+          <Route path="/auth" element={!user ? <Auth />  : <Navigate to='/flashbackapp/posts' />} />
         </Routes>
       </Container>
     </BrowserRouter>
