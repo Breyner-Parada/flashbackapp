f4f1278425ff4745203ac1eb389cc5fef62252d9
Breyner Parada <paradabre@gmail.com>
2024-03-19 11:54:10 -0500

    handle all routes added

--- server/index.js
+++ server/index.js
@@ -15,6 +15,9 @@ app.use(express.static("../public/index.html"));
 app.use(cors());
 routerWeb(app);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to the FLASHBACK API");
