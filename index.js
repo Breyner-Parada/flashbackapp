7803c6b0ab2034593dfd6276aff177bb4416e354
Breyner Parada <paradabre@gmail.com>
2024-03-19 14:25:52 -0500

    express static added

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const port = config.port;
 main();
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.static(path.join(__dirname, "/client/public")));
 app.use(cors());
 routerWeb(app);
 
