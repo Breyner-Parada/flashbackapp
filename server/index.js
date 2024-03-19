5238cc87e852d9648142d70f4346703ae684b150
Breyner Parada <paradabre@gmail.com>
2024-03-19 12:27:03 -0500

    handle all routes updated

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cors());
 routerWeb(app);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../public/index.html"));
+  res.sendFile(path("../public/index.html"));
 });
 
 app.get("/", (req, res) => {
